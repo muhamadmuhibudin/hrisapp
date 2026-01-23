9d68803dd3c82c0940c51945430c11aa3da4cabb
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-01-23 16:35:30 +0700

    chore: add leave requests table migration

---
+++ database/migrations/2026_01_23_093509_create_leave_requests_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('employees');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('reason');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+};

6046bd195f80de121a09e884aff98199dade0059
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-01-23 16:30:52 +0700

    chore: add employees table migration

---
+++ database/migrations/2026_01_23_092842_create_employees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->date('birth_date');
+            $table->date('hire_date');
+            $table->foreignId('department_id')->constrained('departments');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->string('status');
+            $table->decimal('salary', 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
